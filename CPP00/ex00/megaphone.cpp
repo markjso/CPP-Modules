9df8a85ad06e56cd19b2b947d2733bfa03d6c171
markjso <97271827+markjso@users.noreply.github.com>
2023-02-22 15:52:54 +1030

    Add files via upload

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,1 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 15:34:32 by jmarks            #+#    #+#             */
+/*   Updated: 2023/02/22 15:35:35 by jmarks           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <iostream>
+int main(int ac, char **av)
+{
+    std::string msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    if (ac == 1)
+    std::cout << msg;
+    else
+    {
+    for (int i = 1; i < ac; i++)
+        for (int j = 0; av[i][j]; j++)
+        std::cout << (char)toupper(av[i][j]);
+    }
+        std::cout << std::endl;
+}
