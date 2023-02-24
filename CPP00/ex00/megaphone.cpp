5d9f5d5bd7aa7545db547dcfd60948a72f951530
markjso <97271827+markjso@users.noreply.github.com>
2023-02-24 11:20:16 +1030

    Update megaphone.cpp

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -13,14 +13,14 @@
 #include <iostream>
 int main(int ac, char **av)
 {
-    std::string msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::string msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *"; // create a string called msg with text ""
     if (ac == 1)
-    std::cout << msg;
+    std::cout << msg;                                           // if ac == 1 output msg
     else
     {
     for (int i = 1; i < ac; i++)
         for (int j = 0; av[i][j]; j++)
-        std::cout << (char)toupper(av[i][j]);
+        std::cout << (char)toupper(av[i][j]);                   //output the arg at av[i][j] changing it to uppercase
     }
-        std::cout << std::endl;
+        std::cout << std::endl;                                 //output eol
 }
