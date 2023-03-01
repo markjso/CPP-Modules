9f673fd1536644c758a7a9766e0a08984b4b1762
markjso <97271827+markjso@users.noreply.github.com>
2023-03-01 11:15:21 +1030

    Add files via upload

---
+++ CPP00/ex01/Contact.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 10:40:00 by jmarks            #+#    #+#             */
+/*   Updated: 2023/03/01 10:40:09 by jmarks           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+Contact::Contact(){}
+Contact::~Contact(){}
+
+void Contact::displayContactInfo(){
+    std::cout << "\n";
+    std::cout << "INDEX CARD\n";
+    std::cout << "first name: " << firstname << std::endl;
+	std::cout << "last name: " << lastname << std::endl;
+    std::cout << "nickname: " << nickname << std::endl;
+	std::cout << "ph number: " << number << std::endl;
+	std::cout << "darkest secret: " << secret << std::endl;
+}
