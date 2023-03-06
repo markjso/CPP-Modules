5eacc2a2e9bdfb09067190bdb9cef879b74e6cdb
markjso <97271827+markjso@users.noreply.github.com>
2023-03-06 18:34:13 +1030

    Update Contact.cpp

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -61,38 +61,3 @@ void	Contact::setSecret(std::string str)
 {
 	this->secret = str;
 }
-
-// void Contact::getData(){
-//     index = 0;
-//     std::cout << "Enter First Name: \n";
-//     std::getline(std::cin >> std::ws, firstname);
-//     std::cout << "Enter Last Name: \n";
-//     std::getline(std::cin >> std::ws, lastname);
-//     std::cout << "Enter a Nickname: \n";
-//     std::getline(std::cin >> std::ws, nickname);
-//     std::cout << "Enter Phone Number: \n";
-//     std::getline(std::cin >> std::ws, number);
-//     std::cout << "Enter your darkest secret: \n";
-//     std::getline(std::cin >> std::ws, secret);
-//     index++;
-// }
-
-// void Contact::displayContacts(){
-//     // int i = 0;
-//     // for (int i = 0; i < 3; i++)
-// 	//std::cout << std::setw(10) << std::right << i + 1 << "|";
-//     putstr(person.getFirst());
-//     putstr(person.getLast());
-//     putstr(person.getNickname());
-//     std::cout << "\n";
-// }
-
-// void Contact::displayContactInfo(){
-//     std::cout << "\n";
-//     std::cout << "INDEX CARD\n";
-//     std::cout << "first name: " << getFirst() << std::endl;
-// 	std::cout << "last name: " << getLast() << std::endl;
-//     std::cout << "nickname: " << getNickname() << std::endl;
-// 	std::cout << "ph number: " << getNumber() << std::endl;
-// 	std::cout << "darkest secret: " << getSecret() << std::endl;
-// }
