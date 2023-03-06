af5405445bc8fc8bff303435cffe813359eff99d
markjso <97271827+markjso@users.noreply.github.com>
2023-03-06 20:36:58 +1030

    Update PhoneBook.hpp

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -17,6 +17,12 @@
 # include <string>
 # include "Contact.hpp"
 
+/* In the PhoneBook class, there is an array of Contact objects, and each contact is stored at 
+// a specific index in the array. When a new contact is added, it is placed in the first available
+// index in the array. When a contact is displayed, the user is prompted to enter the index of
+// the contact they want to see, and the program retrieves the contact from the array at the 
+// specified index. */
+
 class   PhoneBook {
             Contact person[8];
             int totalContacts;
