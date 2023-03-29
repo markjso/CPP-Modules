cb1616ea8a7e33089ce26368fe57a01448acbae8
markjso <97271827+markjso@users.noreply.github.com>
2023-03-29 11:49:13 +1030

    Update Fixed.hpp

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -15,6 +15,15 @@
 
 # include <iostream>
 
+/* A copy constructor is used to initalise the members of
+a newly created object by copying the members of an 
+already existing object. The object &copy is created 
+taking a reference to an object of the same class as an
+argument. Copy constructor creates new memory storage
+every time it is called whilst the assignment operator does
+not make new memory storage. */
+
+
 class   Fixed{
     private:
             int _raw;
@@ -22,9 +31,9 @@ class   Fixed{
 
         public:
             Fixed( void );
-            Fixed( const Fixed& copy );
+            Fixed( const Fixed& copy );             // copy constructor
             ~Fixed( void );
-            Fixed& operator=( const Fixed& copy );
+            Fixed& operator=( const Fixed& copy );  // copy assignment operator
             int getRawBits( void ) const;
             void setRawBits( int const raw );          
 
