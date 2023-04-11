aa1b4620e9712e5dd8618097c41bb4fae6def034
markjso <97271827+markjso@users.noreply.github.com>
2023-04-11 15:14:51 +0930

    Update and rename AAnimal.hpp to Animal.hpp

--- CPP04/ex02/Animal.hpp
+++ CPP04/ex02/Animal.hpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AAnimal.hpp                                        :+:      :+:    :+:   */
+/*   Animal.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,21 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef AANIMAL_HPP
-# define AANIMAL_HPP
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
 
 # include <iostream>
 
-class   AAnimal {
+class   Animal {
     protected:
             std::string _type;
 
     public:
-        AAnimal ( void );
-        AAnimal ( std::string type );
-        AAnimal ( const AAnimal& copy );
-        virtual ~AAnimal ( void );
-        AAnimal& operator=( const AAnimal& copy );
+        Animal ( void );
+        Animal ( std::string type );
+        Animal ( const Animal& copy );
+        virtual ~Animal ( void );
+        Animal& operator=( const Animal& copy );
         virtual void makeSound() const = 0;	// is now an abstract function
 	std::string getType() const;
 };
