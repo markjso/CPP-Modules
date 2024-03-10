6a95b89f2beef4ff3ec256fa9bf165a46088a2b0
markjso <97271827+markjso@users.noreply.github.com>
2024-03-10 15:29:32 +1030

    Update PmergeMe.hpp

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -20,37 +20,24 @@
 #include <ctime>
 #include <iomanip>
 
-class PmergeMe 
-{
-	private:	
-		PmergeMe(PmergeMe const &copy);
-		PmergeMe &operator=(PmergeMe const &copy);
-		std::vector<int> v_arr;
-		std::deque<int> d_arr;
+class PmergeMe {
+    private:
+    PmergeMe(PmergeMe const &copy);
+	PmergeMe &operator=(PmergeMe const &copy);
+    std::vector<int> _unsortedVector;
+    std::deque<int> _unsortedDeque;
 
-	public:
-		PmergeMe();
-		~PmergeMe();
-		size_t getJacobstahlNumber(size_t n);
-		void initialise(int argc, char **argv);
-		std::vector<std::vector<int> > makePairs(std::vector<int> arr);
-		std::vector<std::vector<int> > sortPairs(std::vector<std::vector<int> >& pairs);
-		void sortPairsIntoHighest(std::vector<std::vector<int> >& pairs);
-		std::vector<int> getSortedVector(const std::vector<std::vector<int> >& myPairs);
-		std::vector<int> getUnsortedVector(const std::vector<std::vector<int> >& myPairs);
-		size_t binarySearch(const std::vector<int>& arr, int insertNum);
-		void binaryInsert(std::vector<int>& arr, int insertNum);
-		void mergeInsertSort(std::vector<int>& mainChain);
-		void printBefore(std::vector<int>& arr);
-    	void printAfter(std::vector<int>& arr);
-		std::deque<std::deque<int> > makePairs(std::deque<int> arr);
-		std::deque<std::deque<int> > sortPairs(std::deque<int> arr);
-		void sortPairsIntoHighest(std::deque<std::deque<int> >& pairs);
-		std::deque<int> getSortedDeque(const std::deque<std::deque<int> >& myPairs);
-		std::deque<int> getUnsortedDeque(const std::deque<std::deque<int> >& myPairs);
-		int binarySearch(const std::deque<int>& arr, int key);
-		void binaryInsert(std::deque<int>& arr, int key);
-		void mergeInsertSort(std::deque<int>& mainChain);
+    std::vector<int> mergeVector(const std::vector<int>& left, const std::vector<int>& right);
+    void mergeSortInsertVector(std::vector<int>& vec);
+    std::deque<int> mergeDeque(const std::deque<int>& left, const std::deque<int>& right);
+    void mergeSortInsertDeque(std::deque<int>& deq);
+
+    public:
+    PmergeMe();
+    ~PmergeMe();
+    void initialise(int argc, char **argv);
+    void printBefore(std::vector<int>& arr);
+    void printAfter(std::vector<int>& arr);
 };
 
 #endif
